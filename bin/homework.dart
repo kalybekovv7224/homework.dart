1add2f440784520636557b47f19fa3d326af3abe
kalybekovv7224 <youremail@yourdomain.com>
2023-09-20 02:06:59 +0600

    first commit

---
+++ bin/homework.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+void main() {
+  // print("Введите первое число");
+//double a = double.parse(stdin.readLineSync()!);
+  // print("Введите второе число");
+  // double b = double.parse(stdin.readLineSync()!);
+  // print(
+  //     "${a + b}-Сложение\n${a - b}-Вичитание\n ${a * b}-Умножение\n ${a / b}-Деление\n");
+
+  // print("Введите число");
+//int a= int.parse(stdin.readLineSync()!);
+  // print(
+  //     "$a * 1 =${a * 1}\n$a * 2 =${a * 2}\n$a * 3 =${a * 3}\n$a * 4 =${a * 4}\n$a * 5 =${a * 5}\n$a * 6 =${a * 6}\n$a * 7 =${a * 7}\n$a * 8 =${a * 8}\n$a * 9 =${a * 9}\n$a * 10 =${a * 10}\n");
+
+  //print("Введите градусы в Цельсиях");
+  //double a = double.parse(stdin.readLineSync()!);
+
+  //print("$a в Цельсиях = ${(a * 9 / 5) + 32} в Фаренгейтах");
+
+  //print("Введите сумму");
+  //double summ = double.parse(stdin.readLineSync()!);
+  //print("Введите название валюты");
+  //String valuta = stdin.readLineSync()!;
+  //print("Введите стоимость валюты");
+  //double price = double.parse(stdin.readLineSync()!);
+  //print("$summ $valuta = ${price * summ} Сом");
+}
